05ad54edd5df3ee72079968a64e01112287ee499
Emiliano Anichini <emiliano.anichini@gmail.com>
2024-04-28 20:35:28 +0200

    changes on linux

--- config/launch.lua
+++ config/launch.lua
@@ -9,7 +9,7 @@ if platform.is_win then
    options.default_prog = { 'pwsh' }
    options.launch_menu = {
       { label = 'Command Prompt', args = { 'cmd' } },
-      { label = 'Nushell',        args = { 'nu' } },
+      { label = 'Nushell', args = { 'nu' } },
    }
 elseif platform.is_mac then
    options.default_prog = { '/usr/local/bin/fish' }
@@ -17,11 +17,9 @@ elseif platform.is_mac then
       { label = 'Nushell', args = { '/usr/local/bin/nu' } },
    }
 elseif platform.is_linux then
-   options.default_prog = { 'fish' }
+   options.default_prog = { '/usr/local/bin/fish' }
    options.launch_menu = {
-      { label = 'Bash', args = { 'bash' } },
-      { label = 'Fish', args = { 'fish' } },
-      { label = 'Zsh',  args = { 'zsh' } },
+      { label = 'Nushell', args = { '/usr/local/bin/nu' } },
    }
 end
 
