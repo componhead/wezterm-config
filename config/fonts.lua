6ffe83bbd456bacb4aa94920e03385b149ada45d
Emiliano Anichini <emiliano.anichini@gmail.com>
2024-06-25 13:31:35 +0200

    change font dimension

--- config/fonts.lua
+++ config/fonts.lua
@@ -2,7 +2,7 @@ local wezterm = require('wezterm')
 local platform = require('utils.platform')
 
 local font = 'Monoid Nerd Font'
-local font_size = platform().is_mac and 22 or 16
+local font_size = platform().is_mac and 16 or 20
 
 return {
    font = wezterm.font(font),
